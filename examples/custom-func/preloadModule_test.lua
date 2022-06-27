6292ac9699851afe39b4d788998669feab571271
Carson <jackparadise520a@gmail.com>
2022-06-27 17:32:45 +0800

    (( link to source code ))

--- examples/custom-func/preloadModule_test.lua
+++ examples/custom-func/preloadModule_test.lua
@@ -2,8 +2,8 @@
 載入我們定義的go函數
 --]]
 
-local m = require("Math2") -- 我們在PreloadModule中所定義的名稱
+local m = require("Math2") -- 我們在PreloadModule中所定義的名稱: https://github.com/CarsonSlovoka/gopher-lua-examples/blob/cd03ce4/examples/custom-func/main.go#L36-L55
 local a = m.add(1,2)
 print(a)
 local b = m.sub(1,2)
-myModule.logPrint(b) -- 這一個是我們之前於RegisterModule所定義的Module
+myModule.logPrint(b) -- 這一個是我們之前於RegisterModule所定義的Module: https://github.com/CarsonSlovoka/gopher-lua-examples/blob/cd03ce4/examples/custom-func/main.go#L17-L30
